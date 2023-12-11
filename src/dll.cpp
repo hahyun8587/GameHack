282d6bd832924c8d857b63be6abff278f6e678a9
Hahyun Baek <hahyun8587@gmail.com>
2023-12-11 16:58:25 +0900

    test stathack

--- src/dll.cpp
+++ src/dll.cpp
@@ -1,16 +1,12 @@
-#include <Windows.h>
-#include <iostream>
 #include "hack.hpp"
 #include "stathack.hpp"
 //#include "triggerhack.hpp"
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-    //MessageBoxW(0, 0, 0, 0);
-    
     if (fdwReason == DLL_PROCESS_ATTACH) {
-        StatHack *th = new StatHack();
+        StatHack *sh = new StatHack();
 
-        th->modify(ASSULT_CUBE);    
+        sh->modify(ASSULT_CUBE);    
     }
     
     return TRUE;
