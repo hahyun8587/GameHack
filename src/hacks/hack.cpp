0c81fcde3cdb0b47a2eebb0de3f249dc82d7f6ca
Hahyun Baek <hahyun8587@gmail.com>
2023-12-11 13:18:37 +0900

    fix type casting by using x86 multilib gcc

--- src/hacks/hack.cpp
+++ src/hacks/hack.cpp
@@ -6,7 +6,7 @@ void Hack::redirect(unsigned char *base, int nnop, void (*codecave)()) {
     VirtualProtect((void *) base, nnop + 4, PAGE_EXECUTE_READWRITE, &old_protect);
 
     *base = JMP;
-    *((DWORD *) (base + 1)) = (DWORD) (reinterpret_cast<void *>(codecave)) - (DWORD) base - 5;
+    *((DWORD *) (base + 1)) = (DWORD) (codecave) - (DWORD) base - 5;
 
     for (int i = 5; i <= nnop + 4; i++) {
         *(base + i) = NOP;
