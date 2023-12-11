de00d99d2b4ecf3fea1e4327e7e5be679536db8e
Hahyun Baek <hahyun8587@gmail.com>
2023-12-11 16:53:02 +0900

    implement stathack

--- src/hacks/stathack.cpp
+++ src/hacks/stathack.cpp
@@ -6,13 +6,13 @@ void StatHack::modify(GameType type) {
             DWORD *static_base;
             DWORD *dynamic_base;
             DWORD *health, *bullet;
-            
+                        
             static_base = (DWORD *) 0x50F4F4;
             dynamic_base = (DWORD *) *static_base;
 
             health = dynamic_base + 0xF8 / 4;
             bullet = (DWORD*) *(DWORD *) (*(dynamic_base + 0x374 / 4) + 0x14);
-
+            
             while (1) {
                 if (*health < 100) {
                     *health = 500;
